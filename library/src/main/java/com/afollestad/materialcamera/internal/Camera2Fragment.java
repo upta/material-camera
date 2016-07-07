8a7da5d604cea498fd26052399cedef559187c0e
Brian Uptagrafft <upta@outlook.com>
2016-07-07 15:53:15 -0700

    meh

--- library/src/main/java/com/afollestad/materialcamera/internal/Camera2Fragment.java
+++ library/src/main/java/com/afollestad/materialcamera/internal/Camera2Fragment.java
@@ -258,7 +258,7 @@ public class Camera2Fragment extends BaseCameraFragment implements View.OnClickL
                     //noinspection ConstantConditions
                     int facing = characteristics.get(CameraCharacteristics.LENS_FACING);
                     if (facing == CameraCharacteristics.LENS_FACING_FRONT)
-                        mInterface.setFrontCamera(cameraId);
+                        mInterface.setFrontCamera(cameraId);DEFAULT_TO_FRONT_FACING
                     else if (facing == CameraCharacteristics.LENS_FACING_BACK)
                         mInterface.setBackCamera(cameraId);
                 }
@@ -289,9 +289,6 @@ public class Camera2Fragment extends BaseCameraFragment implements View.OnClickL
                     }
                 }
             }
-            
-            // give no fucks, use the back camera
-            mInterface.setCameraPosition(CAMERA_POSITION_BACK);
 
             // Choose the sizes for camera preview and video recording
             CameraCharacteristics characteristics = manager.getCameraCharacteristics((String) mInterface.getCurrentCameraId());
