d1f05fbad10b0607c42bfb25a3ea8baa0548b5ea
Brian Uptagrafft <upta@outlook.com>
2016-07-08 11:07:44 -0700

    import

--- library/src/main/java/com/afollestad/materialcamera/internal/BaseCameraFragment.java
+++ library/src/main/java/com/afollestad/materialcamera/internal/BaseCameraFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.media.ToneGenerator;
 
 import com.afollestad.materialcamera.MaterialCamera;
 import com.afollestad.materialcamera.R;
